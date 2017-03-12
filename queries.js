a6ea76d57d4b50de2833299b5bee878714c69d63
Duke Nguyen <nguyentranduc1@gmail.com>
2017-03-11 17:16:47 -0800

    More debugging

--- queries.js
+++ queries.js
@@ -5,13 +5,13 @@ module.exports = {
 		pg.connect(process.env.DATABASE_URL, function(err, client, done) {
 			client.query('SELECT * FROM users;', function(err, result) {
 			done();
-			console.log("RESULT IN QUERIES " + result);
+			console.log("RESULT IN QUERIES " + result.rows);
 			if (err){ 
 				console.error(err); 
 				// response.send("Error " + err); 
 				return null;
 			}else{
-				return result;
+				return result.rows;
 			}
 			});
 		});
@@ -20,3 +20,15 @@ module.exports = {
 
 var zemba = function () {
 }
+
+app.get('/db', function (request, response) {
+ pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+   client.query('SELECT * FROM users;', function(err, result) {
+     done();
+     if (err)
+      { console.error(err); response.send("Error " + err); }
+     else
+      { response.render('pages/db', {results: result.rows} ); }
+   });
+ });
+});
