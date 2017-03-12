a9ed2b2e88560c921d0c84c4a1b5b11f1ed6e9be
Duke Nguyen <nguyentranduc1@gmail.com>
2017-03-11 16:59:19 -0800

    Update new query structure

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ var app = express();
 
 var pg = require('pg');
 
-var tools = require('./queries');
+var queries = require('./queries');
 
 // Database sample call
 app.get('/db', function (request, response) {
-  console.log(request);
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM users', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
+  queries.query(pg, response);
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* Example of a call to database. More info at: https://devcenter.heroku.com/articles/heroku-postgresql#local-setup
 
--- queries.js
+++ queries.js
@@ -1,8 +1,18 @@
+
+
 module.exports = {
-  foo: function () {
-    // whatever
-  },
-  bar: function () {
-    // whatever
+  query: function (pg, response) {
+      pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    	client.query('SELECT * FROM users', function(err, result) {
+      	done();
+      	if (err){ 
+      		console.error(err); response.send("Error " + err); 
+      	}else{ 
+      		response.render('pages/db', {results: result.rows} );}
+	    });
+  	});
   }
 };
+
+var zemba = function () {
+}
