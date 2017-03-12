ad0d6db2adde21fee9cbb77d13825b835018e24d
Duke Nguyen <nguyentranduc1@gmail.com>
2017-03-11 17:17:23 -0800

    Comment out

--- queries.js
+++ queries.js
@@ -21,14 +21,14 @@ module.exports = {
 var zemba = function () {
 }
 
-app.get('/db', function (request, response) {
- pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-   client.query('SELECT * FROM users;', function(err, result) {
-     done();
-     if (err)
-      { console.error(err); response.send("Error " + err); }
-     else
-      { response.render('pages/db', {results: result.rows} ); }
-   });
- });
-});
+// app.get('/db', function (request, response) {
+//  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//    client.query('SELECT * FROM users;', function(err, result) {
+//      done();
+//      if (err)
+//       { console.error(err); response.send("Error " + err); }
+//      else
+//       { response.render('pages/db', {results: result.rows} ); }
+//    });
+//  });
+// });
