91bb539d597c5eb774509697ac241a1e72d98313
Duke Nguyen <nguyentranduc1@gmail.com>
2017-03-11 16:36:18 -0800

    Change table name

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var pg = require('pg');
 // Database sample call
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM users', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
