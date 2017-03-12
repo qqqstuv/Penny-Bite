06671341675e2a78c35e80a2ec09b9f910791826
Duke Nguyen <nguyentranduc1@gmail.com>
2017-03-11 17:05:01 -0800

    Change more structure

--- queries.js
+++ queries.js
@@ -1,19 +1,20 @@
 
 
 module.exports = {
-  query: function (pg) {
-      pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    	client.query('SELECT * FROM users', function(err, result) {
-      	done();
-      	if (err){ 
-      		console.error(err); 
-      		// response.send("Error " + err); 
-      		return null;
-      	}else{
-      		return result;
-	    });
-  	});
-  }
+	query: function (pg) {
+		pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+			client.query('SELECT * FROM users', function(err, result) {
+			done();
+			if (err){ 
+				console.error(err); 
+				// response.send("Error " + err); 
+				return null;
+			}else{
+				return result;
+			}
+			});
+		}
+	}
 };
 
 var zemba = function () {
