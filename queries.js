759eaf49ed9728a91c2f3150983476ae8d02eb03
Duke Nguyen <nguyentranduc1@gmail.com>
2017-03-11 17:10:58 -0800

    semi colon thanks to jordan

--- queries.js
+++ queries.js
@@ -3,7 +3,7 @@
 module.exports = {
 	query: function (pg) {
 		pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-			client.query('SELECT * FROM users', function(err, result) {
+			client.query('SELECT * FROM users;', function(err, result) {
 			done();
 			if (err){ 
 				console.error(err); 
